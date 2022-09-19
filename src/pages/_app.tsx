ac98455ea8e7acc6f7136c7fd34ba540eeebe8dd
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-09-19 20:04:41 +1000

    feat: Build everything up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import { supabaseClient } from '@supabase/auth-helpers-nextjs'
+import { UserProvider } from '@supabase/auth-helpers-react'
 import type { AppProps } from 'next/app'
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider supabaseClient={supabaseClient}>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,49 @@
+import { supabaseClient } from '@supabase/auth-helpers-nextjs'
+import { useUser } from '@supabase/auth-helpers-react'
+import { Auth } from '@supabase/ui'
 import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
+  const { user, error } = useUser()
+  const [data, setData] = useState<any>({})
+
+  useEffect(() => {
+    async function loadData() {
+      const { data } = await supabaseClient.from('test').select('*')
+      setData(data!)
+    }
+
+    if (user) loadData()
+  }, [user])
+
+  if (!user) {
+    console.error(error)
+    return (
+      <>
+        <div className='prose prose-zinc lg:prose-xl mx-auto'>
+          <div className='grid place-items-center h-screen'>
+            <Auth
+              supabaseClient={supabaseClient}
+              socialButtonSize='medium'
+              socialLayout='horizontal'
+            />
+          </div>
+        </div>
+      </>
+    )
+  }
+
   return (
     <>
+      <nav className='flex flex-wrap items-center justify-between p-3'>
+        <div className='flex'>
+          <button onClick={() => supabaseClient.auth.signOut()}> Sign out</button>
+        </div>
+      </nav>
       <div className='prose prose-zinc lg:prose-xl mx-auto'>
         <div className='grid place-items-center h-screen'>
-          <h1>Hello World!</h1>
+          <h1>Welcome User {user?.id}</h1>
         </div>
       </div>
     </>
